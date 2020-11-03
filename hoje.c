1fa6283d5396773346bba33fdf804906359a647f
Bruna <brunamacieira30@gmail.com>
2020-11-03 19:03:14 +0000

    Bruna

---
+++ hoje.c
@@ -0,0 +1,39 @@
+/**
+ * @file hoje.c
+ * @author Bruna Macieira
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    //declarar variaveis
+    int num1, num2;
+    float soma, subtrac, multi, divisao;
+
+    printf("Introduza o seu primeiro numero: \n");
+    scanf("%f", &num1);
+
+    printf("Introduza o seu segundo numero: \n");
+    scanf("%f", &num2);
+
+    soma = num1 + num2;
+    printf("soma= %f", &soma);
+    subtrac = num1 - num2;
+    printf("subtracao= %f", &subtrac);
+    multi = num1 * num2;
+    printf("multiplicacao= %f", &multi);
+    divisao = num1 / num2;
+    printf("divisao= %f", &divisao);
+
+    getchar();
+    return 0;
+
+
+}
